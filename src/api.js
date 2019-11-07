3f5aa48454efccbcfafb82c561589bd145417382
João Vitor Borges <33332428+bobaoapae@users.noreply.github.com>
2019-11-07 11:00:55 -0300

    fix: endpoint

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     //baseURL: "http://192.168.0.104:1100",
-    baseURL: 'http://zapia.com.br:1101',
+    baseURL: 'http://zapia.com.br:1100',
     headers: {'Authorization': "Bearer "+sessionStorage.TOKEN}
 });
 
